8f4153c8cab4cb03a3adb48520bc2533f3ee2baf
kymifBSM <sandmanfin@gmail.com>
2023-05-31 23:41:35 +0700

    task1

---
+++ WPF-laba-16/Menu.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_16
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Options(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Options());
+        }
+
+        private void Credits(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Credits());
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ WPF-laba-16/Options.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_16
+{
+    /// <summary>
+    /// Логика взаимодействия для Options.xaml
+    /// </summary>
+    public partial class Options : Page
+    {
+        public Options()
+        {
+            InitializeComponent();
+        }
+
+        private void color_checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+
+            switch (pressed.Content.ToString())
+            {
+                case "Белый": lbl.Foreground = Brushes.White; break;
+                case "Красный": lbl.Foreground = Brushes.Red; break;
+                case "Голубой": lbl.Foreground = Brushes.Blue; break;
+                case "Зеленый": lbl.Foreground = Brushes.Green; break;
+            }
+        }
+
+        private void OK(object sender, RoutedEventArgs e)
+        {
+            title.Content = box.Text;
+            box.Text = "";
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Menu());
+        }
+    }
+}
